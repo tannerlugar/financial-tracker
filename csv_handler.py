013ad7747d02896a1c7345c8214fdbebcacd7e7f
tannerlugar <tannerlugar.dev@gmail.com>
2025-12-24 00:03:02 -0500

    Implement load_transactions method - CSV read/write complete

--- csv_handler.py
+++ csv_handler.py
@@ -27,3 +27,17 @@ class CSVHandler:
                         "type": transaction.type,
                     }
                 )
+
+    def load_transactions(self) -> list: # Load all transactions from CSV
+        # Create an empty list to store Treansaction objects
+        transactions = []
+        # Open the CSV file for reading
+        with open(self.filename, "r") as f: # 'r' = read mode (does not overwrite)
+            reader = csv.DictReader(f) # Create a CSV reader object
+            for row in reader: # Iterate over each row in the CSV file
+                # Create a new Transaction object from that row's data
+                transaction = Transaction(float(row['amount']), row['date'], row['category'], row['description'], row['type'])
+                transactions.append(transaction) # Add the new Transaction object to the list
+            return transactions # Return the list
+
+
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from csv_handler import CSVHandler
 from tracker import TransactionManager
 from transaction import Transaction
 
+# Create manager and handler
 manager = TransactionManager()
+csv_handler = CSVHandler('data/transactions.csv')
 
-manager.add_transaction(Transaction(-40.00, "12/14/2025", "Car", "Gas", "expense"))
-manager.add_transaction(
-    Transaction(1000.00, "12/01/2025", "Salary", "Paycheck", "income")
-)
+# Add some transactions
+manager.add_transaction(Transaction(-40.00, '12/14/2025', 'Car', 'Gas', 'expense'))
+manager.add_transaction(Transaction(1000.00, '12/01/2025', 'Salary', 'Paycheck', 'income'))
 
-for t in manager.get_all_transactions():
+# Save to CSV
+csv_handler.save_transactions(manager.get_all_transactions())
+print('Saved!')
+
+#Load from CSV
+loaded = csv_handler.load_transactions()
+print('\nLoaded transactions:')
+for t in loaded:
     print(t)
