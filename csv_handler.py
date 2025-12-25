f28b3a25e08a6a37e094f226dcaa861ed85a6f54
tannerlugar <tannerlugar.dev@gmail.com>
2025-12-24 20:20:14 -0500

    Remove transactions.csv from tracking, add to .gitignore

--- csv_handler.py
+++ csv_handler.py
@@ -31,13 +31,17 @@ class CSVHandler:
     def load_transactions(self) -> list: # Load all transactions from CSV
         # Create an empty list to store Treansaction objects
         transactions = []
-        # Open the CSV file for reading
-        with open(self.filename, "r") as f: # 'r' = read mode (does not overwrite)
-            reader = csv.DictReader(f) # Create a CSV reader object
-            for row in reader: # Iterate over each row in the CSV file
-                # Create a new Transaction object from that row's data
-                transaction = Transaction(float(row['amount']), row['date'], row['category'], row['description'], row['type'])
-                transactions.append(transaction) # Add the new Transaction object to the list
-            return transactions # Return the list
+        try:
+            # Open the CSV file for reading
+            with open(self.filename, "r") as f: # 'r' = read mode (does not overwrite)
+                reader = csv.DictReader(f) # Create a CSV reader object
+                for row in reader: # Iterate over each row in the CSV file
+                    # Create a new Transaction object from that row's data
+                    transaction = Transaction(float(row['amount']), row['date'], row['category'], row['description'], row['type'])
+                    transactions.append(transaction) # Add the new Transaction object to the list
+        except FileNotFoundError:
+            # If the file does not exist, return an empty list
+            pass
+        return transactions # Return the list of Transaction objects
 
 
