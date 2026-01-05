5fd493d57e7b7308e0253d42e69f851687f7fff8
tannerlugar <tannerlugar.dev@gmail.com>
2026-01-04 22:03:15 -0500

    Add utility methods and deepen code understanding

--- transaction.py
+++ transaction.py
@@ -10,3 +10,13 @@ class Transaction:
         return "{} | {} | ${:.2f} | {} | {}".format(
             self.date, self.category, self.amount, self.description, self.type
         )
+
+    def is_expense(self) -> bool:
+        return self.type == 'expense'
+    
+    def is_income(self) -> bool:
+        return self.type == 'income'
+
+    def get_absolute_amount(self) -> float:
+        # Return the amount as a positive number (remove negative sign)
+        return abs(self.amount)
